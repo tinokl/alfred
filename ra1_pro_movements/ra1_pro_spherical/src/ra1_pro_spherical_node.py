f169b78e99682441fc83dc7b52971670de8494e7
Konstantin Lassnig <konstantin.lassnig@student.tugraz.at>
2015-01-30 19:02:49 +0100

    working on dyn reconf for python

--- ra1_pro_movements/ra1_pro_spherical/src/ra1_pro_spherical_node.py
+++ ra1_pro_movements/ra1_pro_spherical/src/ra1_pro_spherical_node.py
@@ -5,11 +5,13 @@ import rospy
 import math
 import random
 
+from dynamic_reconfigure.server import Server as DynamicReconfigureServer
+from ra1_pro_spherical.cfg import RA1ProSphericalConfig as ConfigType
+
 from std_msgs.msg import *
 from ra1_pro_msgs.srv import *
 from ra1_pro_msgs.msg import *
 from sensor_msgs.msg import JointState
-
 
 class Ra1ProSpherical:
 
@@ -22,6 +24,11 @@ class Ra1ProSpherical:
         rospy.Subscriber("/ra1_pro/delta_ptn", DeltaPoint, self.delta_point_cb)
         rospy.Subscriber("/joint_states", JointState, self.get_state_cb)
         self.joint_state = rospy.Publisher('/move_group/controller_joint_states', JointState, queue_size=10)
+
+        #client = dynamic_reconfigure.client.Client("RA1ProSperical", timeout=30, config_callback=self.config_cb)
+        self.server = DynamicReconfigureServer(ConfigType, self.config_cb)
+
+        self.config = None
 
         self.joints_horizontal = ['servo_6']
         self.joints_vertical = ['servo_5', 'servo_4', 'servo_3']
@@ -39,18 +46,27 @@ class Ra1ProSpherical:
 
         self.dance_joint = None
         self.dance_alpha = 0
-        self.dance_amplitude = 0.3
+        self.dance_amplitude = 0.2
+        self.dance_incr = 40
 
         self.epsilon_y = 50
         self.epsilon_x = 50
 
-        self.rate = rospy.Rate(10)
+        self.srate = 10
+        self.rate = rospy.Rate(self.srate)
         while not rospy.is_shutdown():
-            if self.is_follow_ptn is True:
+            if self.is_follow_ptn == True:
                 self.follow_point()
-            elif self.is_dancing is True:
+            elif self.is_dancing == True:
                 self.dance_move()
-            #self.rate.sleep()
+            self.rate.sleep()
+
+    def config_cb(config):
+        self.config = config
+        rospy.loginfo(rospy.get_name() + ": I am in")
+        #self.rate = rospy.Rate(config.rate)
+        #self.dance_amplitude = config.dance_amplitude
+        #self.dance_incr = config.dance_incr
 
     def handle_follow_sv(self, req):
         resp = BasicCMDResponse()
@@ -88,7 +104,7 @@ class Ra1ProSpherical:
                 rospy.logerr(rospy.get_name() + ": I was not dancing!")
                 resp = resp.ERROR
         elif req.cmd == req.START:
-            self.rate = rospy.Rate(50)
+            self.rate = rospy.Rate(15)
             if self.is_dancing is False:
                 rospy.loginfo(rospy.get_name() + ": Start to dance!")
                 self.is_dancing = True
@@ -127,7 +143,7 @@ class Ra1ProSpherical:
             index = self.last_state_msg.name.index(joint_name)
             real_angle = self.last_state_msg.position[index]
 
-            d_angle = (math.pi/180.0) * self.last_dp_msg.delta_x/70
+            d_angle = (math.pi/180.0) * self.last_dp_msg.delta_x/50
             d_angle = math.copysign(d_angle, self.last_dp_msg.delta_x) * (-1.0)
             new_angle = real_angle + d_angle
 
@@ -141,7 +157,7 @@ class Ra1ProSpherical:
             index = self.last_state_msg.name.index(joint_name)
             real_angle = self.last_state_msg.position[index]
 
-            d_angle = (math.pi/180.0) * self.last_dp_msg.delta_y/70
+            d_angle = (math.pi/180.0) * self.last_dp_msg.delta_y/50
             d_angle = math.copysign(d_angle, self.last_dp_msg.delta_y)
             new_angle = real_angle + d_angle
 
@@ -158,11 +174,11 @@ class Ra1ProSpherical:
         move_joints.append(joint_name)
         index = self.last_state_msg.name.index(joint_name)
         real_angle = self.last_state_msg.position[index]
-
+        print config["rate"]
         d_angle = math.sin(self.dance_alpha*math.pi/180) * self.dance_amplitude
-        self.dance_alpha += 40
-        if self.dance_alpha > 360:
-            self.dance_alpha = 0
+        self.dance_alpha += self.dance_incr % 360
+        #if self.dance_alpha > 360:
+        #    self.dance_alpha = 0
 
         rospy.loginfo(rospy.get_name() + ": Dance Alpha: " + str(self.dance_alpha))
 
